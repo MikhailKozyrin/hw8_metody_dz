8902da763ff2acee60cac4965a0a41146a361cc3
user <kozyan94@gmail.com>
2022-08-06 16:12:13 +0300

    hw8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
+
+    public static void leapAndPrintYears(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void getAndPrintClientOS(int clientOS, int year2) {
+        if (clientOS == 0 && year2 == 2022) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && year2 < 2022) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && year2 == 2022) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && year2 < 2022) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Ваша ОС не обслуживается");
+        }
+    }
+
+    public static int calculateDeliveryDays(int distance) {
+        if (distance < 20 && distance > 0) {
+            return 1;
+        } else if (distance < 60 && distance >= 20) {
+            return 2;
+        } else if (distance < 100 && distance >= 60) {
+            return 3;
+        } else {
+            throw new RuntimeException("Доставка не может быть произведена");
+        }
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task 1 Високосные года
+        int year = 2017;
+        leapAndPrintYears(year);
+
+        //task 2 OS
+        int clientOS = 1;
+        int year2 = 2020;
+        getAndPrintClientOS(clientOS, year2);
+
+        //task 3 дистанция
+        int deliveryDistance = 95;
+        System.out.println("Количество дней доставки составит: " + calculateDeliveryDays(deliveryDistance));
     }
 }
